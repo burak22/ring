3f1482636f07425ff0c94795f72f577811df8193
burak22 <43789237+burak22@users.noreply.github.com>
2022-09-28 18:56:18 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,10 +4,9 @@ public class Solution {
     public static void main(String[] args) {
         Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
         Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
-        //DEĞİŞİKLİK BURADA
-        Fighter[] fighters = {alex,marc};
-        int randomNm = ((Math.random()*100)>50)? 1:0;
-        Match match = new Match(fighters[randomNm],fighters[1-randomNm], 90 , 100);
+        Fighter[] fighters = {alex,marc};        //DEĞİŞİKLİK BURADA
+        int randomNm = ((Math.random()*100)>50)? 1:0;        //DEĞİŞİKLİK BURADA
+        Match match = new Match(fighters[randomNm],fighters[1-randomNm], 90 , 100);        //DEĞİŞİKLİK BURADA
         match.run();
       }
 }
